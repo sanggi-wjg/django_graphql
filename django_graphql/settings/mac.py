b5485c7c08069bd72f8844ce8a0b7f024e550a6b
Raynor <jonggu.woo@fitpet.co.kr>
2022-10-13 14:43:18 +0900

    :bento: update settings.mac

--- django_graphql/settings/mac.py
+++ django_graphql/settings/mac.py
@@ -1,1 +1,30 @@
 from .base import *
+
+CELERY_BROKER_URL = "redis://127.0.0.1:6379/1"
+# CELERY_RESULT_BACKEND = "redis://15.164.50.32:6379/1"
+CELERY_RESULT_BACKEND = 'django-db'
+CELERY_CACHE_BACKEND = 'django-cache'
+
+CELERY_TIMEZONE = "Asia/Seoul"
+CELERY_ACCEPT_CONTENT = ('application/json',)
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "COMPRESSOR": "django_redis.compressors.zlib.ZlibCompressor",
+            "SOCKET_CONNECT_TIMEOUT": 5,
+            "SOCKET_TIMEOUT": 5,
+            "CONNECTION_POOL_KWARGS": {
+                "max_connections": 100,
+                "retry_on_timeout": True
+            },
+        }
+    }
+}
